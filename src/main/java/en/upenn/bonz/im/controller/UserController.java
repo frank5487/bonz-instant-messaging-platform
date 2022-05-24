df76ea1b73a9e15d645928dcd597ed32128a0118
Yung-Jen <frank5d2008@gmail.com>
2022-05-24 19:17:56 -0400

    show user list

--- src/main/java/en/upenn/bonz/im/controller/UserController.java
+++ src/main/java/en/upenn/bonz/im/controller/UserController.java
@@ -1,9 +1,14 @@
 package en.upenn.bonz.im.controller;
 
+import en.upenn.bonz.im.pojo.Message;
+import en.upenn.bonz.im.pojo.User;
+import en.upenn.bonz.im.pojo.UserData;
 import en.upenn.bonz.im.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,12 +21,35 @@ public class UserController {
     private MessageService messageService;
 
     /**
-     * pull user list
+     * pull user list (mock)
      * @param fromId
      * @return
      */
     @GetMapping
     public List<Map<String, Object>> queryUserList(@RequestParam("fromId") Long fromId) {
-        return null;
+        List<Map<String, Object>> result = new ArrayList<>();
+
+        for (Map.Entry<Long, User> userEntry : UserData.USER_MAP.entrySet()) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("id", userEntry.getValue().getId());
+            map.put("avatar", "https://bonz-house-project.s3.amazonaws.com/avatar/chat_avatar.jpg");
+
+            map.put("from_user", fromId);
+            map.put("info_type", null);
+            map.put("to_user", map.get("id"));
+            map.put("username", userEntry.getValue().getUsername());
+
+            // TODO: get the last message
+            List<Message> messages = messageService.queryMessageList(fromId,
+                    userEntry.getValue().getId(), 1, 1);
+
+            if (messages != null && !messages.isEmpty()) {
+                Message message = messages.get(0);
+                map.put("chat_msg", message.getMsg());
+                map.put("chat_time", message.getSendDate().getTime());
+            }
+            result.add(map);
+        }
+        return result;
     }
 }
